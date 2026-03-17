0e9fa54ce1b4f73007a47898e344abd62fcff747
jayraj rathod <jarathod@bestpeers.in>
2026-03-17 12:47:37 +0530

    created routes for the models

---
+++ backend/src/routes/issue.route.ts
@@ -0,0 +1,26 @@
+// import { Router } from "express";
+
+// const router = Router({ mergeParams: true });
+
+// // Create issue
+// router.post("/", createIssue);
+
+// // Get all issues (filter by sprint, status, assignee)
+// router.get("/", getIssues);
+
+// // Get single issue
+// router.get("/:issueId", getIssueById);
+
+// // Update issue
+// router.put("/:issueId", updateIssue);
+
+// // Delete issue
+// router.delete("/:issueId", deleteIssue);
+
+// // Assign issue
+// router.patch("/:issueId/assign", assignIssue);
+
+// // Change status (todo, in-progress, done)
+// router.patch("/:issueId/status", updateIssueStatus);
+
+// export default router;
---
+++ backend/src/routes/project.route.ts
@@ -0,0 +1,24 @@
+// import { Router } from "express";
+
+// const router = Router();
+
+// // Create project
+// router.post("/");
+
+// // Get all projects (for user/org)
+// router.get("/");
+
+// // Get single project
+// router.get("/:projectId");
+
+// // Update project
+// router.put("/:projectId");
+
+// // Delete project
+// router.delete("/:projectId");
+
+// // Project members
+// router.post("/:projectId/members");
+// router.delete("/:projectId/members/:userId");
+
+// export default router;
